aae7497aca49d1e89dbc74d4564724def4e7f0ec
Jonathan_Ishii <jtishii@yahoo.com>
2018-10-12 19:51:32 -0700

    Made a button like a boss.

---
+++ src/components/MainContainer/Button.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = ({ onPress, children }) => {
+  const { textStyle, buttonStyle } = styles;
+
+  return (
+    // TouchableOpacity expects a prop called onPress
+    <TouchableOpacity onPress={onPress} style={buttonStyle}>
+      <Text style={textStyle}>
+        {children}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    // paddingTop: 10,
+    // paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1, // expand to fit as much content as you can
+    alignSelf: 'stretch', // strech to fill the limits of the container
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    // marginLeft: 5,
+    // marginRight: 5
+  }
+};
+
+export default Button;
