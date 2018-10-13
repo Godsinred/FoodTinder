99ec413edc2dc6315fdb447e31621f5d53854788
brotaku13 <Brian.caulfield13@gmail.com>
2018-10-12 20:23:53 -0700

    made the Match View Container

--- src/components/MatchView/MatchViewContainer.js
+++ src/components/MatchView/MatchViewContainer.js
@@ -1,46 +1,58 @@
 import React, { Component, PropTypes } from 'react';
 // import NavBar from './navbar';
-import { Platform, StyleSheet, Text, View, Button, PropTypes} from 'react-native';
+import { Platform, StyleSheet, Text, View, Button, PropTypes } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
 
 
-import {searchByLocation} from '../../YelpApi/YelpApiFunctions';
-
+import { searchByLocation } from '../../YelpApi/YelpApiFunctions';
+const component1 = () => <Text>Hello</Text>
+const component2 = () => <Text>World</Text>
+const component3 = () => <Text>ButtonGroup</Text>
 export default class AppContainer extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             cardStack: []
         };
     }
 
-    componentWillMount(){
-        searchByLocation(this.props.location).then(results =>{
-            results.business.forEach(business =>{
+    componentWillMount() {
+        searchByLocation(this.props.location).then(results => {
+            results.business.forEach(business => {
                 this.state.cardStack.push(this.createCard(business))
             })
         })
     }
 
-    createCard(business){
+    createCard(business) {
         console.log(`Card created for ${business.name}`);
         return business.id;
     }
 
-    render(){
 
+
+    render() {
+        const buttons = [{ element: component1 }, { element: component2 }, { element: component3 }]
+        return (
+            <ButtonGroup
+                style={styles.buttonGroup}
+                onPress={console.log('here')}
+                buttons={buttons}
+                containerStyle={{ height: 100 }}
+            />
+        )
     }
 
 }
 
 const styles = StyleSheet({
-    container:{
+    container: {
         flex: 1,
         flexDirection: 'column',
         justifyContent: 'flex-start'
     },
-    profileView:{
-        
+    buttonGroup: {
+        justifyContent: 'flex-end'
     }
 })
 
@@ -48,5 +60,5 @@ AppContainer.PropTypes = {
     location: PropTypes.shape({
         latitude: PropTypes.number,
         longitude: PropTypes.number
-      }),
+    }),
 }
